a1e09823b82728228527eade1af30ead95f2f68e
biamonto <markusbiamont@gmail.com>
2019-10-11 23:22:40 +0200

    Create vanilla_option_bs.py
    Script for pricing vanilla options using the black scholes formula

---
+++ vanilla_option_bs.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 11 23:02:12 2019
+
+@author: marku
+"""
+
+import numpy as np
+import scipy.stats as ss
+
+def black_Scholes(type, St, K, sigma, delta_T, r):
+    
+    d1 = 1 / (sigma * np.sqrt(delta_T)) * (np.log(St/K) + (r + sigma**2/2) * delta_T)
+    d2 = d1 - sigma * np.sqrt(delta_T)
+    
+    if type == "Call":
+        price = ss.norm.cdf(d1) * St - ss.norm.cdf(d2) * K * np.exp(-r * delta_T)
+    elif type == "Put":
+        price = ss.norm.cdf(-d2) * K * np.exp(-r * delta_T) - ss.norm.cdf(-d1) * St
+    
+    return price
+
+'''
+type = "Call"
+St = 100
+K = 99
+sigma = 0.3
+delta_T = 1
+r = 0.01
+
+price = black_Scholes(type, St, K, sigma, delta_T, r)
+print(price)
+'''
